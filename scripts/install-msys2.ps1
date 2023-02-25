0d7bd92672acb449761def9e234934c4a9a05129
lonkaars <loek@pipeframe.xyz>
2023-02-25 16:02:35 +0100

    update/fix build config for windows

--- scripts/install-msys2.ps1
+++ scripts/install-msys2.ps1
@@ -1,3 +1,3 @@
-Start-BitsTransfer -Source https://github.com/msys2/msys2-installer/releases/download/2022-09-04/msys2-x86_64-20220904.exe -Destination .\msys2-x86_64-latest.exe
+Start-BitsTransfer -Source https://github.com/msys2/msys2-installer/releases/download/2023-01-27/msys2-x86_64-20230127.exe -Destination .\msys2-x86_64-latest.exe
 .\msys2-x86_64-latest.exe in --confirm-command --accept-messages --root C:/msys64
 Remove-Item .\msys2-x86_64-latest.exe
--- scripts/install-packages-mingw.sh
+++ scripts/install-packages-mingw.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pacman --noconfirm --needed -Sy \
+    base-devel wget curl \
     make git \
     mingw-w64-x86_64-arm-none-eabi-gcc \
     mingw-w64-x86_64-arm-none-eabi-gdb \
@@ -7,6 +8,8 @@ pacman --noconfirm --needed -Sy \
     mingw-w64-x86_64-arm-none-eabi-binutils \
     mingw-w64-x86_64-stlink \
     mingw-w64-x86_64-gdb-multiarch \
+    mingw-w64-x86_64-gcc \
+    mingw-w64-x86_64-SDL2 \
     python python-pip
 
 pip3 install compiledb
