91c9d1c9fc13cf163c6262437de16c992dadaa5b
Flenk008 <frenk_0_0@hotmail.com>
2023-03-13 18:03:21 +0100

    Revert "Background collision"
    This reverts commit e47f7fa198229b8598b8ab03ef8b2483f7c685bc.

--- src/entity.h
+++ src/entity.h
@@ -8,20 +8,7 @@ typedef struct {
 	uint16_t pos_y;
 	uint8_t radius;
 	uint8_t rotation; //45 degrees steps 0 == right 2 == down 4 == left 6 == up
-	int8_t direction_x; //direction where its looking at in case no input;
-	int8_t accelY;
-	int16_t speed;	// default L/R MODifier
-	int16_t air_speed; // air L/R MODifier
+	uint8_t direction_x; //direction where its looking at in case no input;
+	int8_t speed;	//10 default L/R MODifier
 	bool in_air;
 } hh_s_entity_player;
-
-typedef struct {
-	uint16_t pos_x;
-	uint16_t pos_y;
-	uint8_t radius;
-	uint8_t direction_x; //direction where its walking towards;
-	int8_t accelY;
-	int16_t speed;	// default L/R MODifier
-	int16_t air_speed;
-	bool in_air;
-} hh_s_entity_enemy;
