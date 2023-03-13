ec7b1726e60f6077fc90d736e8224b9473534d79
lonkaars <loek@pipeframe.xyz>
2023-03-13 20:38:34 +0100

    untested POC

--- src/main.c
+++ src/main.c
@@ -1,21 +1,37 @@
 #include <stdlib.h>
+#include <stm32f0xx_hal.h>
 
-#include "demo.h"
-#include "input.h"
 #include "main.h"
+#include "stm32/setup.h"
+#include "../test/ppu-stm-integration-demo/data.h"
 
 bool g_hh_run = true;
 
 int main() {
 	hh_setup();
-	hh_loop();
+
+	while (1) {
+		for (unsigned long i = 0; i < HH_PPUINTDEMO_LENGTH; i++) {
+			uint16_t addr = HH_PPUINTDEMO_ADDR[i];
+			uint16_t data = HH_PPUINTDEMO_DATA[i];
+
+			uint8_t spi_data[4] = {
+				(addr & 0xff00) >> 8,
+				(addr & 0x00ff) >> 0,
+				(data & 0xff00) >> 8,
+				(data & 0x00ff) >> 0,
+			};
+
+			HAL_SPI_Transmit(&hspi1, spi_data, 4, HAL_MAX_DELAY);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, true);
+			// HAL_Delay(1);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, false);
+			// HAL_Delay(1);
+		}
+		HAL_Delay(1e3);
+	}
+
 	hh_exit();
 	return 0;
 }
 
-void hh_ppu_vblank_interrupt() {
-	static unsigned long frame = 0;
-	hh_input_read();
-	hh_demo_loop(frame);
-	frame++;
-}
---
+++ test/ppu-stm-integration-demo/readme.md
@@ -0,0 +1,9 @@
+# ppu-stm integration demo
+
+- data.txt is generated using a hacked version of the ppusim demo
+- makefile converts data into header file for c
+- main.c should be compiled and uploaded to stm32
+- this program sends ppu data to the ppu using HAL SPI, and should produce
+  img.png as output
+- other details undocumented
+
