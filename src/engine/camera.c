5c999fb95681ffaff41e59f8f43c82b5cf148e42
UnavailableDev <ggwildplay@gmail.com>
2023-03-14 17:26:17 +0100

    camera y-axis bug fixed + clean up

--- src/engine/camera.c
+++ src/engine/camera.c
@@ -6,10 +6,10 @@
 vec_cor hh_update_camera(vec_cen new, vec2 min, vec2 max){
 
 	//TODO: change floating point math to fix point math
-	//TODO: fix buggy y-axis ??
+	//TODO: remove magic number at y camera offset
 
 	// new = vec_cen2cor(new,(vec2){.x=max.x/2,.y=max.y/2});
-	new = vec_cen2cor((vec2){.x=new.x+(HH_PPU_SPRITE_WIDTH/2),.y=(new.y+(HH_PPU_SPRITE_HEIGHT/2))*2},(vec2){.x=max.x/2,.y=max.y/2});
+	new = vec_cen2cor((vec2){.x=new.x+(HH_PPU_SPRITE_WIDTH),.y=new.y+(HH_PPU_SPRITE_HEIGHT*8)},(vec2){.x=max.x/2,.y=max.y/2});
 	// new.x = new.x << HH_MATH_FIXED_POINT;
 	// new.y = new.y << HH_MATH_FIXED_POINT;
 	static vec_cor old;
@@ -19,6 +19,7 @@ vec_cor hh_update_camera(vec_cen new, vec2 min, vec2 max){
 	// int16_t some = 0;
 	// some = some <<= HH_MATH_FIXED_POINT-1;
 
+	// Camera smoothing
 	new.x = (int)((float)new.x*0.1f + (float)old.x*0.9f);
 	new.y = (int)((float)new.y*0.1f + (float)old.y*0.9f);
 
