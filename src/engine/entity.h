e79c36460f400991caa82598499c0ea7c37ebb90
Flenk008 <frenk_0_0@hotmail.com>
2023-03-15 18:58:25 +0100

    Player-Enemy collision
    Added player with enemy collision. Also changed input system to a vector system with friction and gravity. Jump also works. Enemy is 1 stationary object.

--- src/engine/entity.h
+++ src/engine/entity.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "ppu/types.h"
 
@@ -32,6 +33,8 @@ typedef struct {
 typedef struct {
     vec2 pos, vel, vec;
     bool is_grounded;
+	 bool is_hit;
+	 uint8_t radius;
     int8_t hp;
     int8_t speed;
 	 hh_s_rendering render;
@@ -55,3 +58,25 @@ bool hh_collision(vec2 pos1, vec2 pos2);
 /// @param entity position
 /// @return solved new entity position
 void hh_solve_collision(vec2 pos_environment, hh_entity* entity);
+
+/// @brief solve collision of entity with background tiles
+/// @param temp_old_entity old data of entity 
+/// @param temp_new_entity new data of entity where it wants to go to
+/// @return updated new entity where it actually can go to
+hh_entity hh_background_collision (hh_entity temp_old_entity, hh_entity temp_new_entity);
+
+/// @brief solve collision of player with enemy 
+/// @param temp_player data of player 
+/// @param temp_enemy data of enemy
+/// @return updated player with new stats if hitted with enemy
+hh_entity hh_enemy_collision(hh_entity temp_player, hh_entity temp_enemy);
+
+/// @brief calculate if circles (entity) hit each other 
+/// @param object_1 position of first object (entity)
+/// @param object_2 position of second object (entity)
+/// @param radius_1 radius of first object (entity)
+/// @param radius_2 radius of second object (entity)
+/// @return true if objects collids
+bool hh_distance_circles (vec2 object_1, vec2 object_2, int radius_1, int radius_2);
+
+
--- src/engine/player_controller.c
+++ src/engine/player_controller.c
@@ -11,6 +11,8 @@ void hh_player_actions() {
 		.hp = 4,
 		.speed = 6,
 		.is_grounded = false,
+		.is_hit = false,
+		.radius = 8,
 		.pos = (vec2){32,32},
 		.vel = (vec2){0,0},
 		.vec = (vec2){0,0},
@@ -26,9 +28,113 @@ void hh_player_actions() {
 		}
 	}, player_new = {0};
 
+
+	static hh_entity enemy={
+		.hp = 4,
+		.speed = 6,
+		.is_grounded = false,
+		.is_hit = false,
+		.radius = 8,
+		.pos = (vec2){128,48},
+		.vel = (vec2){0,0},
+		.vec = (vec2){0,0},
+		.render = {
+			.frame0 = 20,
+			.palette = 7,
+			.fam = (hh_s_ppu_loc_fam_entry){
+				.horizontal_flip = false,
+				.vertical_flip = false,
+				.palette_index = 7,
+				.tilemap_index = 1,
+			}
+		}
+	};
+	player_new = player;
 	// hh_input_read();
+	static uint8_t hit = 0;
+	int8_t hit_timer = 0;
+	int8_t direction_x = (-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right);
+	int8_t direction_y = (-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down);
+
+	if(player.is_hit == true){
+		hit_timer = 9;
+		player.is_hit = false;	
+	} 
+	if(hit_timer > -10){
+			hit_timer--;
+	}
+
+	if(hit_timer <= 0){
+		if(direction_x != 0){
+			if(player.vel.x > -1 * player.speed && player.vel.x < player.speed) {
+				player.vel.x = player.vel.x + direction_x;
+			} else {
+				if (player.vel.x > 0) {
+					player.vel.x--;
+				} else if(player.vel.x < 0) {
+					player.vel.x++; 
+				}
+			}
+		} else {
+			if (player.vel.x > 0) {
+				player.vel.x--;
+			} else if(player.vel.x < 0) {
+			player.vel.x++; 
+			}
+		}
+		
+		/* // movement Y (w-s) disable gravity to use this
+		if(direction_y != 0){
+			if(player.vel.y > -4 && player.vel.y < 4 ) {
+				player.vel.y =  player.vel.y + direction_y;
+			}
+		} else {
+			if (player.vel.y > 0) {
+				player.vel.y--;
+			} else if(player.vel.y < 0) {
+				player.vel.y++; 
+			}
+		}		
+		
+		*/
+	} else {
+		if (player.vel.x > 0) {
+			player.vel.x--;
+		} else if(player.vel.x < 0) {
+			player.vel.x++; 
+		}
+			player.vel.y++;
+	}
+	
+
+	if (g_hh_controller_p1.button_primary && player.is_grounded == true) {//JUMP
+	 	player.vel.y = -10;
+		player.is_grounded = false;
+	} else if (player.vel.y < 6){
+		 player.vel.y += 1; //gravity
+	}
+
+
+
+	
+/*
 	player.vel = (vec2){.x = (-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right),
 	.y = (-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down) };
+
+		player_new.vel = (vec2){
+		.x = player.vel.x,
+		.y = player.vel.y,
+	};
+*/
+
+		player_new.vel = (vec2){
+		.x = player.vel.x,
+		.y = player.vel.y,
+	};
+
+		player_new = hh_enemy_collision(player, enemy);
+	
+	
 	// const int8_t maa = 3;
 	// const int8_t mbb = -3;
 	// if (g_hh_controller_p1.dpad_up)
@@ -55,8 +161,8 @@ void hh_player_actions() {
 	// player.vel.x = CLAMP(player.vel.x,-32,32);
 
 	player_new.pos = (vec2){
-		.x = player.pos.x + player.vel.x,
-		.y = player.pos.y + player.vel.y,
+		.x = player.pos.x + player_new.vel.x,
+		.y = player.pos.y + player_new.vel.y,
 	};
 
 
@@ -96,41 +202,10 @@ void hh_player_actions() {
 	// 		hh_solve_collision(tiles[i].pos, &player);
 	// 	}
 	// }
-
-	player_new.is_grounded = false;
-
-	// solves x collision
-	if (player.vel.x <= 0) {
-		if (hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 0, .y=player.pos.y + 0})) || 
-			hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 0, .y=player.pos.y + 15}))) {
-			player_new.pos.x = (player_new.pos.x & ~15) + 16,
-			player_new.vel.x = 0;
-		}
-	} else {
-		if (hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 16, .y=player.pos.y + 0})) || 
-			hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 16, .y=player.pos.y + 15}))) {
-			player_new.pos.x = player_new.pos.x & ~15, // <-- magic comma, NOT TOUCHY
-			player_new.vel.x = 0;
-		}
-	}
-
-	//solves y collision
-	if (player.vel.y <= 0) {
-		if (hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 0, .y=player_new.pos.y + 0})) || 
-			hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 0, .y=player_new.pos.y + 15}))) {
-			player_new.pos.y = (player_new.pos.y & ~15) + 16,
-			player_new.vel.y = 0;
-		}
-	} else {
-		if (hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 0, .y=player_new.pos.y + 16})) || 
-			hh_colidable(hh_world_to_tile((vec2){.x=player_new.pos.x + 16, .y=player_new.pos.y + 15}))) {
-			player_new.pos.y = player_new.pos.y & ~15,
-			player_new.vel.y = 0;
-			player_new.is_grounded = true;
-		}
-	}
-
-	player = player_new;
+	
+	player =  hh_background_collision ( player, player_new);
+	
+	//player = player_new;
 
 	vec_cor cam_pos;//value in tiles
 	// cam_pos = (vec2){0,0};
@@ -141,10 +216,16 @@ void hh_player_actions() {
 	player.render.fam.position_x = (player.pos.x-cam_pos.x);
 	player.render.fam.position_y = (player.pos.y-cam_pos.y);
 
+	enemy.render.fam.position_x = (enemy.pos.x-cam_pos.x);
+	enemy.render.fam.position_y = (enemy.pos.y-cam_pos.y);
 
 	player.render.fam.tilemap_index = 2;//TODO: these two lines should be redundant
 	player.render.fam.palette_index = 7;
 	hh_ppu_update_foreground(0, player.render.fam);
 
+	hh_ppu_update_foreground(1, enemy.render.fam);
+
 }
+
+
 
