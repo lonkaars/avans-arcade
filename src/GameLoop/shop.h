54c3e1139e3e0e328f7ce3e8a2a61b0bf530a772
UnavailableDev <ggwildplay@gmail.com>
2023-03-22 10:05:46 +0100

    merge

---
+++ src/GameLoop/shop.h
@@ -0,0 +1,16 @@
+#include "input.h"
+#include "engine/draw_screen.h"
+
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+	hh_e_STATE_SHOW,
+	hh_e_STATE_Input,
+	hh_e_STATE_END
+} hh_e_ShopStates;
+
+
+bool hh_show_Shop();
